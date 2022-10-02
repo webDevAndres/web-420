66f42b0943bddb8fd3325851acf9384e1b0c7024
webDevAndres <macias.andres02@gmail.com>
2022-10-01 21:18:48 -0500

    added composer put and delete

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ let http = require("http");
 let swaggerUi = require("swagger-ui-express");
 let swaggerJsdoc = require("swagger-jsdoc");
 let mongoose = require("mongoose");
-// let composerAPI = require("./routes/macias-composer-routes");
+let composerAPI = require("./routes/macias-composer-routes");
 // let personAPI = require("./routes/macias-person-routes");
 // let userAPI = require("./routes/macias-session-routes");
-let customerAPI = require("./routes/macias-node-shopper-routes");
+// let customerAPI = require("./routes/macias-node-shopper-routes");
 let app = express();
 let bodyParser = require("body-parser");
 
@@ -24,7 +24,8 @@ let bodyParser = require("body-parser");
 app.set('port', process.env.PORT || 3000);
 app.set(express.json());
 // app.set(express.urlencoded({'extended': true}));
-app.use(bodyParser.urlencoded({'extended': false}))
+app.use(bodyParser.urlencoded({'extended': false}));
+app.use(bodyParser.json()); //this is needed to have swagger pass the json to the node api
 
 let conn = 'mongodb+srv://web420_user:s3cret@buwebdev-cluster-1.pldlt.mongodb.net/web420DB?retryWrites=true&w=majority';
 mongoose.connect(conn, {
@@ -52,10 +53,10 @@ const options = {
 let openapiSpecification = swaggerJsdoc(options);
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
-// app.use('/api', composerAPI);
+app.use('/api', composerAPI);
 // app.use('/api', personAPI);
 // app.use('/api', userAPI);
- app.use('/api', customerAPI)
+//  app.use('/api', customerAPI);
 
 
 http.createServer(console.log("Application started and listening to port 3000"), app).listen(process.env.PORT || 3000);
--- routes/macias-composer-routes.js
+++ routes/macias-composer-routes.js
@@ -14,7 +14,7 @@ let Composer = require('../models/macias-composer');
 /**
 * openapi: 3.0.0
 * @openapi
-*   /api/composers:
+* /api/composers:
 *     get:
 *       summary: returns a list of all composers
 *       description: API for returning a list of composers from MongoDB Atlas
@@ -73,7 +73,7 @@ router.get('/composers', async (req, res) => {
 */
 router.get('/composers/:id', async (req, res) => {
     try {
-        Composer.findOne({'_id': req.params.id }, function (err, composer) {
+        Composer.findOne({ '_id': req.params.id }, function (err, composer) {
             if (err) {
                 console.log(err);
                 res.status(501).send({
@@ -122,15 +122,14 @@ router.get('/composers/:id', async (req, res) => {
 *           '501':
 *               description: MongoDB Exception
 */
-
 router.post('/composers', async (req, res) => {
     try {
         let newComposer = {
-                firstName: "Gary",
-                lastName: "Smith"
-            };
+            firstName: req.body.firstName,
+            lastName: req.body.lastName
+        };
 
-       await Composer.create(newComposer, function (err, composer) {
+        await Composer.create(newComposer, function (err, composer) {
             if (err) {
                 console.log(err);
                 res.status(501).send({
@@ -148,6 +147,135 @@ router.post('/composers', async (req, res) => {
         });
     };
 });
+
+/**
+* @openapi
+* /api/composers/{id}:
+*     put:
+*       summary: updates the information of a composer
+*       description: API for updating a single composer object from MongoDB
+*       operationId: updateComposerById
+*       parameters:
+*         - name: id
+*           in: path
+*           description: The composer requested by the user
+*           required: true
+*           schema:
+*             type: string
+*       requestBody:
+*           description: composer information
+*           content:
+*               application/json:
+*                   schema:
+*                       type: object
+*                       required:
+*                           - firstName
+*                           - lastName
+*                       properties:
+*                           firstName:
+*                               type: string
+*                           lastName:
+*                               type: string
+*       responses:
+*         '200':
+*           description: Composer document
+*         '500':
+*           description: Server Exception
+*         '501':
+*           description: MongoDB Exception
+*/
+router.put('/composers/:id', async (req, res) => {
+    try {
+        const composerId = req.params.id;
+
+        Composer.findOne({ '_id': composerId }, function (err, composer) {
+            if (err) {
+                console.log(err);
+                res.status(501).send({
+                    'message': `MongoDB Exception: ${err}`
+                });
+            } else {
+                console.log(req.body);
+                if (composer) {
+                    composer.set({
+                        firstName: req.body.firstName,
+                        lastName: req.body.lastName
+                    });
+
+                    composer.save(function (err, updatedComposer) {
+                        if (err) {
+                            console.log(err);
+                            res.json(updatedComposer);
+                        } else {
+                            console.log(updatedComposer);
+                            res.json(updatedComposer);
+                        }
+                    });
+                }
+
+                if (!composer) {
+                    res.status(401).send({
+                        'message': `Invalid composerId`
+                    });
+                }
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            'message': `Server Exception: ${error.message}`
+        });
+    };
+});
+
+/**
+* @openapi
+* /api/composers/{id}:
+*     delete:
+*       summary: removes a document from mongoDB
+*       description: API for deleting a single composer document from MongoDB
+*       operationId: deleteComposerByID
+*       parameters:
+*         - name: id
+*           in: path
+*           required: true
+*           description: id of the document to remove
+*           schema:
+*             type: string
+*       responses:
+*         '200':
+*           description: document removed
+*         '500':
+*           description: Server Exception
+*         '501':
+*           description: MongoDB Exception
+*/
+
+router.delete('/composers/:id', async (req, res) => {
+    try {
+        const composerId = req.params.id;
+
+        Composer.findByIdAndDelete({ '_id': composerId }, function (err, composer) {
+            if (err) {
+                console.log(err);
+                res.status(501).send({
+                    'message': `MongoDB Exception: ${err}`
+                });
+            } else {
+                console.log(composer);
+                res.status(200).send({
+                    'message': `Composer ${req.params.id} has been removed`
+                });
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            'message': `Server Exception: ${error.message}`
+        });
+    };
+});
+
 
 module.exports = router;
 
